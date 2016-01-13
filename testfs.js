20be32a02fa8bfab337d75465970393c93dc2d5c
Paul-Emmanuel Vandenburie <pevandenburie@yahoo.fr>
2016-01-13 09:50:54 +0100

    Try fs pipe.

--- testfs.js
+++ testfs.js
@@ -3,11 +3,12 @@ var fs = require('fs');
 require('http').createServer( function(req, res) {
 
 	var rs = fs.createReadStream("testnode.js");
-	rs.on('data', function(data) {
+	/*rs.on('data', function(data) {
 		res.write(data);
 	});
 	rs.on('end', function() {
 		res.end();
-	});
+	});*/
+	rs.pipe(res);
 }).listen(8888);
 
