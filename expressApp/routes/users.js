b3bd56664dda2b1bd757b1b56524b305b6930fed
Paul-Emmanuel Vandenburie <pevandenburie@yahoo.fr>
2016-01-14 14:09:34 +0100

    Express.js: get the user list from Mongodb using Mongoose, display also the age.

--- expressApp/routes/users.js
+++ expressApp/routes/users.js
@@ -1,26 +1,81 @@
 var express = require('express');
 var router = express.Router();
 
-var users = {
-  	'users': [
-  	{	'name': 'Smith',
-  		'firstname': 'Paul',
-  	},
-  	{	'name': 'Redford',
-  		'firstname': 'Robert',
-  	},
-  	{	'name': 'Trump',
-  		'firstname': 'Donald',
-  	}
-  	]
-  };
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/users');
+
+var db = mongoose.connection;
+console.log("connected");
+
+
+// Create the User schema
+var userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	firstname: {
+		type: String,
+		required: true,
+	},
+	age: {
+		type: Number,
+		required: true,
+	},
+});
+
+// Register the schema
+var User = mongoose.model('User', userSchema);
+
+function addUser(name, firstname, age, callback) {
+	var u = new User({name: name, firstname: firstname, age: age});
+	u.save(function(err, res) {
+		if (err) { 
+			console.log("Error: " + err);
+		}
+		else {
+			console.log(res);
+		}
+
+		if (typeof callback === 'function') {
+			callback(err, res);
+		}
+	});
+}
+
+// Do this only once to populate the DB
+// addUser("Smith", "Paul", 30);
+// addUser("Simon", "Pierre", 35);
+// addUser("Redford", "Robert", 60);
+// addUser("Trump", "Donald", 65);
+
+
+// var users = {
+//   	'users': [
+//   	{	'name': 'Smith',
+//   		'firstname': 'Paul',
+//   	},
+//   	{	'name': 'Redford',
+//   		'firstname': 'Robert',
+//   	},
+//   	{	'name': 'Trump',
+//   		'firstname': 'Donald',
+//   	}
+//   	]
+//   };
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
   //req.app => we can access the app if needed
 
-  res.render('users', users);
+  User.find(function(err, users) {
+  	if (err) {
+  		console.log("Error: " + err);
+  	}
+  	console.log(users);
+  	res.render('users', {users: users});
+  });
 });
 
 module.exports = router;
