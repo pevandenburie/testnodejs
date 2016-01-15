c8f3318e1bc8b2683e4992f46f771e8408881123
Paul-Emmanuel Vandenburie <pevandenburie@yahoo.fr>
2016-01-15 09:07:27 +0100

    Express.js: added mocha test to add a user

--- expressApp/test/test.service.js
+++ expressApp/test/test.service.js
@@ -8,7 +8,7 @@ var operations = userServiceConstructor(UserModel.User);
 describe('Test userService', function() {
 	it('getAll must return plenty of users', function(done) {
 		operations.getAll(function(users) {
-			assert.equal(25, users.length);	// we know in advance the content of the DB
+			assert.equal(26, users.length);	// we know in advance the content of the DB
 			done();
 		});
 	});
@@ -28,17 +28,26 @@ describe('Test userService', function() {
 		var nameToTest = "Trump";
 		var firstnameToTest = "Donald";
 		operations.remove(nameToTest, function(users) {
-			assert.equal(24, users.length);	// we know in advance the content of the DB
+			assert.equal(25, users.length);	// we know in advance the content of the DB
 			done();
 		});
 	});
 
-	// it('add must add a user', function(done) {
-	// 	var nameToTest = "Trump";
-	// 	var firstnameToTest = "Donald";
-	// 	var ageToTest = 65;
-	// 	var u = new UserModel.User(nameToTest, firstnameToTest, ageToTest);
-	// })
+	it('add must add a user', function(done) {
+		var nameToTest = "Trump";
+		var firstnameToTest = "Donald";
+		var ageToTest = 65;
+		//var u = new UserModel.User(nameToTest, firstnameToTest, ageToTest);
+		var u = {
+			name: nameToTest,
+			firstname: firstnameToTest,
+			age: ageToTest
+		};
+		operations.add(u, function(users) {
+			assert.equal(26, users.length);	// we know in advance the content of the DB
+			done();
+		});
+	})
 });
 
 
